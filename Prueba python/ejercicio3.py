df8f71e38398acbfaffa40af30b547c40fd2c756
Pala <andrespala2000@gmail.com>
2018-05-01 09:06:04 -0500

    De java a python

---
+++ Prueba python/ejercicio3.py
@@ -0,0 +1,7 @@
+print ("Tranformar minutos y segundos")
+seg=int(input("Ingrese los segundos: "))
+seg2=int
+m=int
+m=seg/60
+seg2=seg - (m*60)
+print(" segundos es igual a "+minutos+" minutos y "+segundos2+" segundos")
